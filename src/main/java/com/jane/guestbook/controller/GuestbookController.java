75d8034e70445da77316cb06fa6677263cb727f5
yeona98 <sy4454@naver.com>
2021-01-25 15:02:11 +0900

    feat ⚡️ set controller & view

---
+++ src/main/java/com/jane/guestbook/controller/GuestbookController.java
@@ -0,0 +1,20 @@
+package com.jane.guestbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j2
+public class GuestbookController {
+
+    @GetMapping({"/", "/list"})
+    public String list() {
+
+        log.info("show list page");
+
+        return "/guestbook/list";
+    }
+}
