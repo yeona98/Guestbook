f584c66a224068154c21ab12671007389556970c
yeona98 <sy4454@naver.com>
2021-01-26 09:22:29 +0900

    feat ⚡ Querydsl Test

--- src/test/java/com/jane/guestbook/repository/GuestbookRepositoryTest.java
+++ src/test/java/com/jane/guestbook/repository/GuestbookRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.jane.guestbook.repository;
 
 import com.jane.guestbook.entity.Guestbook;
+import com.jane.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -73,5 +76,62 @@ public class GuestbookRepositoryTest {
         System.out.println(result.get().getModDate());
         assertNotEquals(result.get().getModDate(), result.get().getRegDate());
         assertThat(result.get().getModDate()).isAfter(result.get().getRegDate());
+    }
+
+    // Querydsl 테스트
+
+    @Test
+    public void 단일항목검색_쿼리_테스트() {
+        //given
+        Sort sort = Sort.by("gno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort);
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; // 동적 처리를 위한 Q도메인 클래스
+
+        String keyword = "1"; // title 에 "1"이라는 글자가 있는 것 검색
+
+        //when
+        BooleanBuilder builder = new BooleanBuilder(); // where 문에 들어가는 조건들을 넣어주는 컨테이너
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        //then
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+            assertThat(guestbook.getTitle()).contains(keyword);
+        });
+    }
+
+    @Test
+    public void 다중항목검색_쿼리_테스트() {
+        //given
+        Sort sort = Sort.by("gno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort);
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; // 동적 처리를 위한 Q도메인 클래스
+
+        String keyword = "1"; // title 과 content 에 "1"이라는 글자가 있는 것 검색
+
+        //when
+        BooleanBuilder builder = new BooleanBuilder(); // where 문에 들어가는 조건들을 넣어주는 컨테이너
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exAll = exTitle.or(exContent);
+
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L)); // gno 가 0 보다 큰 것 검색
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        //then
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+            assertThat(guestbook.getTitle()).contains(keyword);
+            assertThat(guestbook.getContent()).contains(keyword);
+            assertThat(guestbook.getGno()).isGreaterThan(0L);
+        });
     }
 }
