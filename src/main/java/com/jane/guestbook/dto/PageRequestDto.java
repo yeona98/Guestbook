c06036c5f768a1506d04c7faad1cc086b914337e
yeona98 <sy4454@naver.com>
2021-01-27 08:21:30 +0900

    fix 🔥 fix PageRequestDto error

--- src/main/java/com/jane/guestbook/dto/PageRequestDto.java
+++ src/main/java/com/jane/guestbook/dto/PageRequestDto.java
@@ -1,12 +1,15 @@
 package com.jane.guestbook.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-@Getter
+@Builder
+@AllArgsConstructor
+@Data
 public class PageRequestDto {
     private int page;
     private int size;
@@ -18,11 +21,5 @@ public class PageRequestDto {
 
     public Pageable getPageable(Sort sort) {
         return PageRequest.of(page - 1, size, sort); // 페이지 번호가 0부터 시작
-    }
-
-    @Builder
-    public PageRequestDto(int page, int size) {
-        this.page = page;
-        this.size = size;
     }
 }
